70cf4a4f722d6ca31ff8f199f0440c4e74b1199e
Belvah <misshuey3@gmail.com>
2026-03-27 19:28:44 +0300

    Update 2_1.py to use input() for user sentence

--- 2_1.py
+++ 2_1.py
@@ -1,6 +1,6 @@
 #Write a program that takes a sentence and then prints the number of words in that sentence.
 
-sentence = 'I am a dog mom'
+sentence = input("Enter a sentence: ")
 words = sentence.split()
 print(sentence)
 print("The number of words in the sentence is:", len(words))
