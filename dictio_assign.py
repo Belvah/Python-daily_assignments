fde50303c1e422a6fd87670c17e41cc6bf45650d
Belvah <misshuey3@gmail.com>
2026-03-31 22:46:18 +0300

    Add files

---
+++ dictio_assign.py
@@ -0,0 +1,7 @@
+#Write a Python program to sum up all the items in a dictionary.
+
+my_dict = {'a': 10, 'b': 20, 'c': 30, 'd': 40}
+
+total = sum(my_dict.values())
+
+print( total)
---
+++ prod_information.py
@@ -0,0 +1,18 @@
+#Create a dictionary to store product information (name, price, quantity). Allow users to add, update, and remove items from the inventory.
+
+inventory = {} #initialize an empty dictionary to store product information
+
+def add_product(name, price, quantity): #parameter name, price, quantity
+    
+    inventory[name] = {"price": price, "quantity": quantity} #uses the name as the key and a nested dictionary to store price and quantity
+
+def update_product(name, price=None, quantity=None):
+    if name in inventory:
+        if price is not None:
+            inventory[name]["price"] = price
+        if quantity is not None:
+            inventory[name]["quantity"] = quantity
+
+def remove_product(name):
+    if name in inventory:
+        del inventory[name]
