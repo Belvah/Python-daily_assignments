b2895ee056c20c02eca7bb0b7a7ea8da6d24399f
Belvah <misshuey3@gmail.com>
2026-04-01 18:08:29 +0300

    2.1 assignment

--- prod_information.py
+++ prod_information.py
@@ -3,7 +3,7 @@
 inventory = {} #initialize an empty dictionary to store product information
 
 def add_product(name, price, quantity): #parameter name, price, quantity
-    
+
     inventory[name] = {"price": price, "quantity": quantity} #uses the name as the key and a nested dictionary to store price and quantity
 
 def update_product(name, price=None, quantity=None):
@@ -16,3 +16,5 @@ def update_product(name, price=None, quantity=None):
 def remove_product(name):
     if name in inventory:
         del inventory[name]
+
+       
